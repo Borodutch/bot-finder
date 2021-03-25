99319ea3f861436c89ba2fee0fa346df3ef3c283
Stas N <stas-nepomn@ya.ru>
2021-03-25 19:04:45 +0800

    popup behavior

--- src/content/content.ts
+++ src/content/content.ts
@@ -18,23 +18,37 @@ const runScript = async () => {
   );
 
   for (let username of commentNames) {
-    const botButton = document.createElement('div');
-    botButton.className = '__vcbf comments__item__reply';
-    const userLink = username
-      .getElementsByClassName('comments__item__user')[0]
-      .getAttribute('href');
-    botButton.innerHTML = 'Это бот?';
-    botButton.onclick = async () => {
-      botButton.innerHTML = 'Загрузка...';
-      const userData = await parseUser(userLink);
-      alert(
-        `Reg: ${userData.header.stats[0].label} \n Name: ${userData.header.subsiteData.name}, karma: ${userData.header.subsiteData.karma}, comments: ${userData.header.tabs[1].counter}, articles: ${userData.header.tabs[0].counter}, isPlus: ${userData.header.subsiteData.isPlus}`
-      );
+    const userDom = username.getElementsByClassName('comments__item__user')[0];
+    const userLink = userDom ? userDom.getAttribute('href') : null;
+
+    if (userLink) {
+      const botButton = document.createElement('div');
+      const botDropdown = document.createElement('div');
+      botDropdown.className = '__vcbf__popup';
+      botDropdown.style.display = 'none';
+      botButton.className = 'comments__item__reply __vcbf';
       botButton.innerHTML = 'Это бот?';
-    };
-    username
-      .getElementsByClassName('comments__item__self')[0]
-      .appendChild(botButton);
+      botButton.onclick = async () => {
+        if (botDropdown.style.display === 'none') {
+          botDropdown.innerHTML = 'Загрузка...';
+          botDropdown.style.display = 'block';
+          const userData = await parseUser(userLink);
+          botDropdown.innerHTML = `${userData.header.subsiteData.name} (${
+            userData.header.subsiteData.isPlus ? 'Премиум, ' : ''
+          }карма: ${userData.header.subsiteData.karma})<br /><br />${
+            userData.header.stats[0].label
+          }<br /><br />Комментариев: ${
+            userData.header.tabs[1].counter
+          }<br>Статей: ${userData.header.tabs[0].counter}`;
+        } else {
+          botDropdown.style.display = 'none';
+        }
+      };
+      username
+        .getElementsByClassName('comments__item__self')[0]
+        .appendChild(botButton);
+      username.getElementsByClassName('__vcbf')[0].appendChild(botDropdown);
+    }
   }
 };
 
