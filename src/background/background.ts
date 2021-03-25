a597c2e26fe436955d912437b9d47f01bb486ea2
Stas N <stas-nepomn@ya.ru>
2021-03-25 21:58:48 +0800

    default behavior

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,3 +1,7 @@
+chrome.runtime.onInstalled.addListener(function (details) {
+  chrome.storage.local.set({ bots: true });
+});
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(
